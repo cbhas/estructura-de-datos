49be7457acf3311b7d11d049797d3658229e7a73
Sebastián Calderón <calderonsebastian211@gmail.com>
2023-07-12 23:11:17 -0500

    edd

--- Semana13/src/semana13/ArbolBinario.java
+++ Semana13/src/semana13/ArbolBinario.java
@@ -1,6 +1,53 @@
 package semana13;
 
 public class ArbolBinario {
+    Nodo raiz;
+
+    private Nodo eliminarRecursivo(Nodo actual, int valor) {
+        if (actual == null) {
+            return null;
+            
+        } else if (valor == actual.valor) {
+            if (actual.izquierdo == null && actual.derecho == null) {
+                return null;
+            }
+
+            if (actual.derecho == null) {
+                return actual.izquierdo;
+            }
+
+            if (actual.izquierdo == null) {
+                return actual.derecho;
+            }
+
+            int valorMasPequeño = encontrarValorMasPequeño(actual.derecho);
+            actual.valor = valorMasPequeño;
+            actual.derecho = eliminarRecursivo(actual.derecho, valorMasPequeño);
+            return actual;
+            
+        } else if (valor < actual.valor) {
+            actual.izquierdo = eliminarRecursivo(actual.izquierdo, valor);
+            return actual;
+            
+        } else {
+            actual.derecho = eliminarRecursivo(actual.derecho, valor);
+            return actual;
+        }
+
+    }
+
+    private int encontrarValorMasPequeño(Nodo raiz) {
+        if (raiz.izquierdo == null) {
+            return raiz.valor;
+
+        } else {
+            return encontrarValorMasPequeño(raiz.izquierdo);
+        }
+
+    }
+
+    public void eliminar(int valor) {
+        raiz = eliminarRecursivo(raiz, valor);
     
     Node root;
 
